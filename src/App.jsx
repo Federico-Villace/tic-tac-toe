59c43dcecc609275dfd8e01290564b7dc0f06ed0
Federico Villace <federicovillace@gmail.com>
2023-01-18 23:12:46 -0300

    Added localStorage to save the match

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,39 @@ import confetti from "canvas-confetti";
 import Winner from "./components/Winner";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X;
+  });
   const [winner, setWinner] = useState(null);
 
   const reset = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
   };
 
   const updateBoard = (index) => {
     if (board[index] || winner) return;
+
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
+
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", newTurn);
+
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
       confetti();
